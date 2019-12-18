0340fda97b18a08c73b82c4b008af2d5a36b8bd6
Jesus Alcala <jesus.alcala@transunion.com>
2019-12-18 09:45:24 -0500

    Limpieza del repositorio

--- back-end/stg-services/stg-web-services/src/main/java/com/systig/api/repository/PreguntasRepository.java
+++
@@ -1,8 +0,0 @@
-package com.systig.api.repository;
-
-import com.systig.api.models.Preguntas;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PreguntasRepository extends JpaRepository<Preguntas, Integer> {
-    Preguntas findByBase(boolean b);
-}
